8723c154b7917fde33e15d0c38d7793ede994c3c
roberttce <r.centen0ba@gmail.com>
2024-12-24 21:39:27 -0500

    [add] actualizacion del frontend

--- frontend-alquiler-canchas/src/app/app.component.spec.ts
+++ frontend-alquiler-canchas/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ProyFacchas' title`, () => {
+  it(`should have the 'Facchas' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProyFacchas');
+    expect(app.title).toEqual('Facchas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ProyFacchas');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Facchas');
   });
 });
